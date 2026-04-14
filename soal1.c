96dd33994913532501beb5094f1c90e8d7071235
Rafi Fathia <rafifathia@gmail.com>
2026-04-14 09:06:14 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main(void) {
+int main (){
+    int N;
+    int mod2;
+    int mod3;
+    int count;
+    scanf("%d", &N);
+    mod2 = N%2;
+    mod3 = N%3;
+
+    while (N>0)
+    {
+        if (mod2==0 && mod3==0){
+            printf("BIRU\n");
+        }
+        else if (mod3==0){
+            printf("MERAH\n");
+        }
+        else if (mod2==0){
+            printf("KUNING\n");
+        }
+        else{
+            printf("%d", N);
+        }
+
+    }
     return 0;
 }
